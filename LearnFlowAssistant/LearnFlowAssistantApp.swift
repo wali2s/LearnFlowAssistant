da05046adc574d8c7b6ddb6d1bf1a860d2b26108
Wahid <wahid-ali90@hotmail.com>
2026-06-30 17:55:07 +0200

    Initial Commit

---
+++ LearnFlowAssistant/LearnFlowAssistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnFlowAssistantApp.swift
+//  LearnFlowAssistant
+//
+//  Created by Wahid on 30.06.26.
+//
+
+import SwiftUI
+
+@main
+struct LearnFlowAssistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
